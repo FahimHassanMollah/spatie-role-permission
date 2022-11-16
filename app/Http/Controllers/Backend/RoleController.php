2640826520c49b8b3149868b44d668cacb29e58a
FahimHassanMollah <fahimhassan0197@gmail.com>
2022-11-16 09:57:08 +0600

    laravel project setup,template setup,spatie role permission install,seeder create,role create

---
+++ app/Http/Controllers/Backend/RoleController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roles = Role::all();
+        return view('backend.pages.roles.index',['roles' => $roles]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $permissions = Permission::all();
+        return view('backend.pages.roles.create',['permissions' => $permissions]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        // dd($request->permissions);
+        $request->validate([
+            'name' => 'required | unique:roles,name'
+        ]);
+
+       $role = Role::create(['name' => $request->name]);
+       if ($request->permissions) {
+        $role->syncPermissions($request->permissions);
+       }
+       return back();
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/seeders/RolerPermissionSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolerPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        // create roles
+        $roleSuperAdmin = Role::create(['name' => 'superadmin']);
+        $roleAdmin = Role::create(['name' => 'admin']);
+        $roleEditor = Role::create(['name' => 'editor']);
+        $roleUser = Role::create(['name' => 'user']);
+
+        // Permission List as array
+        $permissions = [
+
+            // Dashboard
+            'dashboard.view',
+
+            // Blog Permissions
+            'blog.create',
+            'blog.view',
+            'blog.edit',
+            'blog.delete',
+            'blog.approve',
+
+            // Admin Permissions
+            'admin.create',
+            'admin.view',
+            'admin.edit',
+            'admin.delete',
+            'admin.approve',
+
+            // Role Permissions
+            'role.create',
+            'role.view',
+            'role.edit',
+            'role.delete',
+            'role.approve',
+
+            // Profile Permissions
+            'profile.view',
+            'profile.edit'
+        ];
+
+        foreach ($permissions as $permission) {
+            $createdPermission = Permission::create(['name' => $permission]);
+            $roleSuperAdmin->givePermissionTo($createdPermission);
+            $createdPermission->assignRole($roleSuperAdmin);
+
+        }
+
+
+
+
+        // assign permission
+    }
+}
